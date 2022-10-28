785e57a9fa8532bb8dd3d2637d8517eeab43cc1d
freetimezp <kraftukrnet@ukr.net>
2022-10-28 20:36:01 +0300

    12. js-services-01-cleaning. Responsive view. Show, hide menu on small devices.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ const headerBg = () => {
 }
 
 headerBg();
+
+//nav 
+const navigation = () => {
+  const navToggler = document.querySelector(".js-nav-toggle");
+  const nav = document.querySelector(".js-nav");
+  const navItems = nav.querySelectorAll("li");
+
+  const navToggle = () => {
+    nav.classList.toggle("open");
+    navToggler.classList.toggle("active");
+  }
+
+  navToggler.addEventListener("click", navToggle);
+
+  navItems.forEach((li) => {
+    li.querySelector("a").addEventListener('click', () => {
+      if(window.innerWidth <= 767) {
+        navToggle();
+      }
+    });
+  });
+};
+navigation();
